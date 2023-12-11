7d43e0e7d59cee4cb0c2517d8caf35a5c67303c1
Ian Bull <irbull@eclipsesource.com>
2023-12-10 21:44:37 -0800

    AoC Dec 9

---
+++ src/bin/dec_9_1.rs
@@ -0,0 +1,51 @@
+fn difference_vec(seq: Vec<i32>) -> Vec<i32> {
+    let mut result = vec![];
+    for i in 0..seq.len() - 1 {
+        result.push(seq[i + 1] - seq[i]);
+    }
+    result
+}
+
+fn is_zeros(seq: &Vec<i32>) -> bool {
+    for i in seq {
+        if *i != 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn solve_seq(seq: Vec<i32>) -> i32 {
+    let mut sol_space: Vec<Vec<i32>> = vec![];
+    let mut last: Vec<i32> = seq.clone();
+    while !is_zeros(&last) {
+        sol_space.insert(0, last.clone());
+        last = difference_vec(last.clone());
+    }
+    sol_space.insert(0, last.clone());
+    sol_space[0].push(0);
+    for i in 1..sol_space.len() {
+        let last_prev_row = sol_space[i - 1].last().unwrap().to_owned();
+        let cur_row = &mut sol_space[i];
+        cur_row.push(last_prev_row + cur_row.last().unwrap());
+    }
+    sol_space.last().unwrap().last().unwrap().to_owned()
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = include_str!("../input/dec_9_1.txt").lines();
+
+    let mut total = 0;
+    for line in input {
+        let seq = line
+            .split_whitespace()
+            .into_iter()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+        let next_num = solve_seq(seq);
+        total += next_num;
+    }
+    println!("total: {}", total);
+
+    Ok(())
+}
